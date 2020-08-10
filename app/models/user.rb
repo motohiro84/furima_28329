cb8d6231d27339d2ef94c7735cd794157526cfad
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-10 17:54:16 +0900

    バリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :products
+  has_many :orders
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。" }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。" }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい。" }
+    validates :birthday
+    validates :password, format: { with: /(?=.*?[a-z])(?=.*?[\d])/i, message: 'は英字と数字の両方を含めて入力してください' }
+    validates :password_confirmation
+    validates :email, uniqueness: true
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
   resources :products
+  resources :users, only: :show
+
 end
