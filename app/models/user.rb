2ecb34dc6779e51f61396846d342820e9203b424
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-11 16:24:46 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,21 @@ class User < ApplicationRecord
   has_many :products
   has_many :orders
 
+
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  NAME＿READING_REGEX = /\A[ァ-ヶー－]+\z/
+  HALF_REGEX = /\A[a-zA-Z0-9]+\z/
+  MIXTURE_REGEX = /(?=.*?[a-z])(?=.*?[\d])/i
+
   with_options presence: true do
     validates :nickname
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください。" }
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "は全角で入力してください" }
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力して下さい" }
+    validates :first_name, format: { with: NAME_REGEX, message: "は全角で入力してください。" }
+    validates :last_name, format: { with: NAME_REGEX, message: "は全角で入力してください" }
+    validates :first_name_reading, format: { with: NAME＿READING_REGEX, message: "は全角カタカナで入力して下さい" }
+    validates :last_name_reading, format: { with: NAME＿READING_REGEX, message: "は全角カタカナで入力して下さい" }
     validates :birthday
-    validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: 'は半角の英数字で入力して下さい' }
-    validates :password, format: { with: /(?=.*?[a-z])(?=.*?[\d])/i, message: 'は英字と数字の両方を含めて入力してください' }
+    validates :password, format: { with: HALF_REGEX, message: 'は半角の英数字で入力して下さい' }
+    validates :password, format: { with: MIXTURE_REGEX, message: 'は英字と数字の両方を含めて入力してください' }
     validates :password_confirmation
     validates :email, uniqueness: true
   end
