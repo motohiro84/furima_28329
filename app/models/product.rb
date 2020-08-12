4db028dd6898fe01ccd13a37a35a1e0e77c76426
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-12 15:38:03 +0900

    enumの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,36 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
   belongs_to :order
   has_one :address
   has_one_attached :image
+  belongs_to_active_hash :cate
+  belongs_to_active_hash :prefecture
+
+
+
+  enum condition:{
+    '---':           1,
+    unused:          2,
+    nearly_unused:   3,
+    not_injured:     4,
+    bit_injured:     5,
+    injured:         6,
+    bad:             7,
+  }
+
+  enum postage:{
+    '---'             1,
+    prepayment:       2,
+    freight_collect:  3,
+  }
+
+  enum shipment:{
+    '---'   1,
+    1_to_2  2,
+    2_to_3  3,
+    4_to_7  4,
+  }
+
 end
