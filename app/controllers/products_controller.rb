70a7eb72089e94ac9ed3865082c6e6778edafbd7
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-12 12:38:24 +0900

    未ログイン時のリダイレクト

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,28 @@
 class ProductsController < ApplicationController
 
+  before_action :move_to_index, except: [:index]
+
   def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit()
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
