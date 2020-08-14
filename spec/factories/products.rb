70580fbe6a65d7f024f847f72c0902184b88ee5a
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-14 20:25:35 +0900

    factorybotの作成

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :product do
-    
+    title         {Faker::Lorem.paragraph_by_chars(number: 10 )}
+    text          {Faker::Lorem.sentence}
+    price         {Faker::Number.within(range: 300..9999999)}
+    category_id   {Faker::Number.within(range: 2..48)}
+    prefecture_id {Faker::Number.within(range: 2..11)}
+    condition     {Faker::Number.within(range: 2..7)}
+    postage       {Faker::Number.within(range: 2..3)}
+    shipment      {Faker::Number.within(range: 2..4)}
+    association :user 
   end
 end
