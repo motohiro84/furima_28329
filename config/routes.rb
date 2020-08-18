4fc8a393302654ce0f0ade2f5019bc94bb235750
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-18 19:35:49 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'products#index'
-  resources :products
-  resources :orders, only:[:index, :new, :create]
-
+  resources :products do
+    resources :orders, only:[:index, :new, :create]
+  end
+  
 end
