f0dffc628679a6ad1025ae9e421678008bf29467
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-19 12:37:08 +0900

    Revert "クレジット決済のjavascript"
    This reverts commit d7f043c39c706c02142a02814408fc24c3fa5812.

--- Gemfile
+++ Gemfile
@@ -75,8 +75,6 @@ gem 'image_processing', '~> 1.2'
 
 gem 'active_hash'
 
-gem 'payjp'
-
 group :production do
   gem 'rails_12factor'
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,6 @@
 class OrdersController < ApplicationController
 
   def index
-    @product = Product.find(params[:product_id])
   end
 
   def new
