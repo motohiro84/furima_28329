f87313f691c2175b4b19b84ccf539de11e74e3d1
motohiro84 <motohiro84@users.noreply.github.com>
2020-08-29 11:19:35 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/products/#{comment.product.id}"
+    @comment = Comment.create(comment_params)
+    redirect_to "/products/#{@comment.product.id}"
   end
 
   private
