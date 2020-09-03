d0f11b6b908caf5a9186ad986266e3d21a8936e0
motohiro84 <motohiro84@users.noreply.github.com>
2020-09-03 21:37:46 +0900

    cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = 'ws://18.180.120.243cable'
+  config.action_cable.url = 'ws://18.180.120.243/cable'
   config.action_cable.allowed_request_origins = [ 'http//18.180.120.243' ]
 end
